4bee9c2a7a0d17682687ea93e77fe4f67e9a0745
kylestockton <kylestoc@gmail.com>
2017-01-05 02:30:41 -0800

    added existing files

---
+++ phones.py
@@ -0,0 +1,11 @@
+phonelist = [
+['Barnhart, Ralph', '683-2341'],
+['Bobo, Timothy', '789-6543'],
+['Jones, Janet', '483-5432'],
+['Meyers, Chris', '343-4349'],
+['Nelson, Eric', '485-2689'],
+['Prefect, Ford', '987-6543'],
+['Smith, Bob', '689-1234'],
+['Smith, Robert', '689-1234'],
+['Zigler, Mary', '567-8901']
+]
